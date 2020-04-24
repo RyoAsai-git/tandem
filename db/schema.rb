50d4a1d792768821a978f4a418fe7cc60c97e67d
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-25 01:35:44 +0900

    userカラム追加
    名前、自己紹介、性別、ユーザー画像を設定するカラム追加
    
    ストロングパラメーター設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_154322) do
+ActiveRecord::Schema.define(version: 2020_04_24_162836) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2020_04_24_154322) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", limit: 100, default: "", null: false
+    t.string "self_introduction", limit: 500
+    t.integer "sex", default: 0, null: false
+    t.string "img_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
