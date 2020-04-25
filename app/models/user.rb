f1fbe0f3ed3816077dda1218b3ccbbe498839dc0
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-25 17:01:12 +0900

    ユーザー情報編集機能
    editページフロント
    devise_parameter_sanitizer記述
    
    enumを利用し、ユーザーの性別を数字にて管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   mount_uploader :img_name, ImgNameUploader
+  enum sex: { 男: 0, 女: 1 }
 end
