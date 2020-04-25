0c09050b9ddf6b1bb48813d8d8a770d82a6281c6
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-25 18:25:40 +0900

    test
    db:seed　ダミーデータ追加にて確認
    gitignore dummy_img

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "create Users"
+User.create!(
+  email: 'user1@example.com',
+  password: 'aaaaaaaa',
+  name: 'オリバー',
+  self_introduction: 'オリバーです',
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/1.jpg")
+)
+User.create!(
+  email: 'user2@example.com',
+  password: 'aaaaaaaa',
+  name: 'ハリー',
+  self_introduction: 'ハリーです',
+  sex: 0,
+  img_name: open("#{Rails.root}/db/dummy_img/2.jpg")
+)
+User.create!(
+  email: 'user3@example.com',
+  password: 'aaaaaaaa',
+  name: 'オリヴィア',
+  self_introduction: 'オリヴィアです',
+  sex: 1,
+  img_name: open("#{Rails.root}/db/dummy_img/3.jpg")
+)
+User.create!(
+  email: 'user4@example.com',
+  password: 'aaaaaaaa',
+  name: 'リリー',
+  self_introduction: 'リリーです',
+  sex: 1,
+  img_name: open("#{Rails.root}/db/dummy_img/4.jpg")
+)
