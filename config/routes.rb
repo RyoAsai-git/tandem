eef350df9a3952ae340e52908506c8f32bcff7f7
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-28 17:58:24 +0900

    フォロー機能追加
    コントローラー、ルーティング作成
    テーブル定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: :registrations }
   root "top#index"
   resources :users
+  resources :relationships, only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_162836) do
+ActiveRecord::Schema.define(version: 2020_04_28_082433) do
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,6 @@ ActiveRecord::Schema.define(version: 2020_04_24_162836) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
