cd4c56a94e4491d6a5a990520ec70e27bc31699b
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-29 13:37:16 +0900

    Revert "フォロー機能追加"
    This reverts commit eef350df9a3952ae340e52908506c8f32bcff7f7.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :img_name, ImgNameUploader
   enum sex: { 男: 0, 女: 1 }
-
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
-
-  def follow(other_user)
-    unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
-    end
-  end
-
-  def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user.id)
-    relationship.destroy if relationship
-  end
-
-  def following?(other_user)
-    self.followings.include?(other_user)
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_082433) do
-
-  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "follow_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["follow_id"], name: "index_relationships_on_follow_id"
-    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
-    t.index ["user_id"], name: "index_relationships_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_04_24_162836) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,6 +28,4 @@ ActiveRecord::Schema.define(version: 2020_04_28_082433) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "relationships", "users"
-  add_foreign_key "relationships", "users", column: "follow_id"
 end
