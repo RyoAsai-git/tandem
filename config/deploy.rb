2e972347299c46fd4432bc6c95a6cdd21e8a7ff2
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-30 17:28:50 +0900

    自動デプロイ設定
    capristrano導入
    設定

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+
+lock '3.12.1'
+
+set :application, 'tandem'
+
+set :repo_url,  'git@github.com:RyoAsai-git/tandem.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' 
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/tandem.pem'] 
+                  
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
