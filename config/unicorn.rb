bdb04d8d97620e3d5936fa40c0fea4ad1582d56d
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-30 18:05:15 +0900

    fixed
    capistrano編集

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,12 @@
-
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
 
-working_directory "#app_path/current"
-
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+# それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
