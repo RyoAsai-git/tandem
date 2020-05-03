01878cb57114b9560b9e7e1e45e5380f7f526f31
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-05-03 11:59:52 +0900

    自動デプロイ修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,9 @@
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
-# currentを指定
+
 working_directory "#{app_path}/current"
 
-# それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
